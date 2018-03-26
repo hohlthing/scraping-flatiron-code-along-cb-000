83c12008c9045f04230e875523b4cbfd67ecd9d7
michael hohl <mhohl@rocketmail.com>
2018-03-26 20:01:25 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,17 +3,16 @@ class Course
 
   @@all = []
 
-    def initialize
-      @@all << self
-    end
-
-    def self.all
-      @@all
-    end
-
-    def self.reset_all
-      @@all.clear
-    end
-
+  def initialize
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
 end
